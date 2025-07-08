c26d63583a005a9cc13c41813453f6251d0dfcaa
Domi2004VR <domycar8@gmail.com>
2025-07-08 13:06:43 +0200

    Login Page Scheme

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,14 +1,40 @@
-import AuthCard from "../components/AuthCard";
-import LoginForm from "../components/LoginForm";
+import BlobLoginPage from "../assets/blobLogin.svg"
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import "../styles/LoginPage.css";
 
 function LoginPage() {
-    return (
-        <div className="LoginPage">
-            <AuthCard title="Benvenuto">
-                <LoginForm />
-            </AuthCard>
+    return(
+        <div className="login-page">
+            <div className="blobDiv">
+                <img id="blob-svg" src={BlobLoginPage}/>
+            </div>
+            <div className="formDiv">
+                <div className="InputDiv">
+                    <h1>Benvenuto!</h1>
+
+                    <form>
+                    <FloatingLabel controlId="floatingEmail" label="Email"  className="mb-3">
+                        <Form.Control type="email" placeholder="email" />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingPassword" label="Password"  className="mb-3">
+                        <Form.Control type="password" placeholder="Password" />
+                    </FloatingLabel>
+                    </form>
+
+                    <Button variant="outline-primary" className="button">Login</Button>
+
+                    <p>Non sei ancora registrato? <a href="http://localhost:3000/registrati">Registrati!</a> </p>
+
+                </div>
+
+
+            </div>
+
         </div>
-    );
+
+    )
 }
 
 export default LoginPage;
