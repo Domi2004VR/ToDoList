8ed0af49f9a413e68a115e6ce60c785cead93b35
Diego <diegotto.300503@gmail.com>
2025-07-11 14:37:23 +0200

    Risolto problema Popup lasciando gestire a APP apertura dei Popup e permettendo ai figli solo di aprire e chiudere mandando i dati da inserire

--- frontend/src/components/PopupWindow.jsx
+++ frontend/src/components/PopupWindow.jsx
@@ -12,20 +12,20 @@ function PopupWindow ({popupInfo}) {
         setCode(e.target.value);
     }
 
-
+    console.log("PopupWindow è stata avviata ma non è renderizzata");
     return (
         <div className="popupOverlay">
             <div className="popupContainer">
                 <h2 className="popupTitle">{popupInfo.title}</h2>
                 <p className="popupDescription">{popupInfo.message}</p>
-                {popupInfo.inputText?<div className="inputContainer">
+                {popupInfo.inputText ?<div className="inputContainer">
                     <FloatingLabel controlId="floatingEmail" label="Inserici il codice"  className=" inputContainer mb-3" >
                                 <Form.Control id="popupInput" type="text" placeholder="Inserici il codice" name="code" value={code} onChange={handleChange} />
                             </FloatingLabel>
                     </div> : null}
                 <div className="popupButtonsContainer">
                     <Button id="confirmButton" name="conferma" variant="outline-success" onClick={popupInfo.handleConfirm}>Conferma</Button>
-                    <Button id="closeButton" name="annulla" variant="outline-danger" onClick={popupInfo.handleClose}>Annulla</Button>
+                    <Button id="closeButton" name="annulla" variant="outline-danger" onClick={popupInfo.handleClose} >Annulla</Button>
                 </div>
             </div>
         </div>
